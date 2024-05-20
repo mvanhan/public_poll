d6ac38c9bb56b2101a0836b61306e90e26d11c8f
Miller Van Hanken <mvanhan@unc.edu>
2024-05-20 17:09:51 -0500

    pictures

--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { database, ref, set, increment, onValue } from './firebaseConfig';
+import './Counter.css'; // Import the CSS file
 
 const Counter = () => {
   const [trumpCount, setTrumpCount] = useState(0);
@@ -9,15 +10,14 @@ const Counter = () => {
     const trumpCountRef = ref(database, 'counts/trump');
     const bidenCountRef = ref(database, 'counts/biden');
 
-    // Update the count elements in real-time
     onValue(trumpCountRef, (snapshot) => {
       const count = snapshot.val();
-      setTrumpCount(count);
+      setTrumpCount(count || 0);  
     });
 
     onValue(bidenCountRef, (snapshot) => {
       const count = snapshot.val();
-      setBidenCount(count);
+      setBidenCount(count || 0);  
     });
   }, []);
 
@@ -32,41 +32,21 @@ const Counter = () => {
   };
 
   return (
-    <div style={styles.container}>
-      <div style={styles.buttonContainer}>
-        <button style={styles.button} onClick={handleTrumpClick}>Trump</button>
-        <div style={styles.count}>{trumpCount}</div>
-      </div>
-      <div style={styles.buttonContainer}>
-        <button style={styles.button} onClick={handleBidenClick}>Biden</button>
-        <div style={styles.count}>{bidenCount}</div>
+    <div className="container">
+      <div className="buttonsRow">
+        <div className="buttonContainer">
+          <img src="/Public Poll Trump.jpeg" alt="Trump" className="image" />
+          <button className="button" onClick={handleTrumpClick}>Trump</button>
+          <div className="count">{trumpCount}</div>
+        </div>
+        <div className="buttonContainer">
+          <img src="/Public Poll Biden.jpeg" alt="Biden" className="image" />
+          <button className="button" onClick={handleBidenClick}>Biden</button>
+          <div className="count">{bidenCount}</div>
+        </div>
       </div>
     </div>
   );
-};
-
-const styles = {
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100vh',
-    backgroundColor: '#f0f0f0',
-  },
-  buttonContainer: {
-    margin: '20px',
-  },
-  button: {
-    padding: '20px',
-    fontSize: '20px',
-    margin: '10px',
-    cursor: 'pointer',
-  },
-  count: {
-    fontSize: '24px',
-    marginTop: '10px',
-  },
 };
 
 export default Counter;
